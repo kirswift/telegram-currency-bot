a17314e175b6b06af6e676bb8ca788a3f5947b20
Kirill Savchenko <kirsav10@gmail.com>
2020-07-10 23:28:22 +0400

    Обработка обновлений.

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import requests
+import time
 
 class Bot:
     def __init__(self, token):
         self._url = f'https://api.telegram.org/bot{token}/'
+
+    def start(self):
+        offset = 0
+        while True:
+            updates = self.get_updates(offset)
+            if len(updates) > 0:
+                for update in updates:
+                    self.send_message(120751938, 'pong')
+                offset = self.get_last_update_id(updates) + 1
+            time.sleep(1)
+
+    def get_updates(self, offset):
+        params = {'timeout': 60, 'offset': offset}
+        return requests.get(self._url + 'getUpdates', params).json()['result']
+
+    def get_last_update_id(self, updates):
+        return updates[len(updates) - 1]['update_id']
 
     def send_message(self, chat_id, text):
         params = {'chat_id': chat_id, 'text': text}
@@ -10,4 +28,4 @@ class Bot:
 
 
 bot = Bot('1233613509:AAGYxH3vYQ7WYFz1NlzVFX3DI9GFCwVk0fs')
-bot.send_message(120751938, 'test')
+bot.start()
