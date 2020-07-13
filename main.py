76ba6761630f5e32ec82dcd3896cfb605d28e214
Kirill Savchenko <kirsav10@gmail.com>
2020-07-13 13:11:31 +0400

    Фикс запроса курса.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from telegram import Bot
 from converter import Converter
 
+converter = Converter()
 bot = Bot('1233613509:AAGYxH3vYQ7WYFz1NlzVFX3DI9GFCwVk0fs')
 
 def help_handler(chat_id, args):
     bot.send_message(chat_id, '*Вы ввели команду /help*')
 
 bot.add_command('/help', help_handler)
+
+print(converter.get_exchange_rate('EUR', 'RUB'))
 
 bot.start()
